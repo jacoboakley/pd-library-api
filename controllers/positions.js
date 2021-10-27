8b7edb0b8ab815635f141b38d2b9a8079f92b4b1
Jacob Oakley <jacob@oakley.codes>
2021-10-27 06:29:43 -0500

    Move logic to controller

---
+++ controllers/positions.js
@@ -0,0 +1,54 @@
+import express from "express";
+import Position from "../models/Position.js";
+// Create
+export const addPosition = async (req, res) => {
+    const { description, title } = req.body;
+    const position = new Position({
+        description,
+        title,
+    });
+    
+    try {
+        const savedPosition = await position.save();
+        res.json(savedPosition) 
+    } catch (error) {
+        res.json({message: error})
+    }
+    
+};
+// Read
+export const getAllPositions = async (req, res) => {
+    try {
+        const positions = await Position.find();
+        res.json(positions);
+    } catch (error) {
+        res.json({message: error})
+    }
+};
+
+export const getPositionById = async (req, res) => {
+    try {
+        const foundPosition = await Position.findById(req.params.positionId);
+        res.json(foundPosition);
+    } catch (error) {
+        res.json({message: error})
+    }
+};
+// Update
+export const updatePositionById = async (req, res) => {
+    try {
+        // const deletedPosition = await Position.deleteOne({ _id: req.params.positionId });
+        res.json(deletedPosition);
+    } catch (error) {
+        res.json({message: error})
+    }
+};
+// Delete
+export const deletePositionById = async (req, res) => {
+    try {
+        const deletedPosition = await Position.deleteOne({ _id: req.params.positionId });
+        res.json(deletedPosition);
+    } catch (error) {
+        res.json({message: error})
+    }
+};
--- routes/positions.js
+++ routes/positions.js
@@ -1,48 +1,21 @@
 import express from "express";
-import Position from "../models/Position.js";
+import {
+    addPosition,
+    deletePositionById,
+    getAllPositions,
+    getPositionById,
+    updatePositionById
+} from "../controllers/positions.js";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        const positions = await Position.find();
-        res.json(positions);
-    } catch (error) {
-        res.json({message: error})
-    }
-});
-
-router.get("/:positionId", async (req, res) => {
-    try {
-        const foundPosition = await Position.findById(req.params.positionId);
-        res.json(foundPosition);
-    } catch (error) {
-        res.json({message: error})
-    }
-})
-
-router.delete("/:positionId", async (req, res) => {
-    try {
-        const deletedPosition = await Position.deleteOne({ _id: req.params.positionId });
-        res.json(deletedPosition);
-    } catch (error) {
-        res.json({message: error})
-    }
-})
-
-router.post("/", async (req, res) => {
-    const { description, title } = req.body;
-    const position = new Position({
-        description,
-        title,
-    });
-
-    try {
-        const savedPosition = await position.save();
-        res.json(savedPosition) 
-    } catch (error) {
-        res.json({message: error})
-    }
-    
-})
+// Create
+router.post("/", addPosition);
+// Read
+router.get("/", getAllPositions);
+router.get("/:positionId", getPositionById);
+// Update
+router.patch("/:positionId", updatePositionById);
+// Delete
+router.delete("/:positionId", deletePositionById);
 
 export default router;
